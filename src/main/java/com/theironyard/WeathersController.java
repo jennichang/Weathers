74955cc6ad95d422fa9c5494f4039b4141886ee0
Jennifer Chang <jennichang02@gmail.com>
2016-10-05 16:18:08 -0400

    error in pushing to heroku

--- src/main/java/com/theironyard/WeathersController.java
+++ src/main/java/com/theironyard/WeathersController.java
@@ -1,6 +1,6 @@
 package com.theironyard;
 
-import jodd.json.JsonParser;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpMethod;
